5d4d308b2b8466de60726de18c81071961c81593
sondos emara <sondos.emara2002@gmail.com>
2024-09-26 16:01:19 +0300

    Add Infrastructure Extensions

--- OrderSystem.Infrastructure/Extentions/InfrastructureExtensions.cs
+++ OrderSystem.Infrastructure/Extentions/InfrastructureExtensions.cs
@@ -1,5 +1,14 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Couchbase.Extensions.DependencyInjection;
+using Google.Api;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Graph.Models;
+using OrderSystem.Infrastructure.DB.Classes;
+using OrderSystem.Infrastructure.DB.Interfaces;
+using OrderSystem.Infrastructure.Repositries.Classes;
+using OrderSystem.Infrastructure.Repositries.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +19,31 @@ namespace OrderSystem.Infrastructure.Configuration
 {
     public static class InfrastructureExtensions
     {
-        public static IServiceCollection AddInfrastructureDependendies(this IServiceCollection services, IConfiguration configuration)
+      
+        public static IServiceCollection AddInfrastructureBuilder(this IServiceCollection services, IConfiguration configuration)
         {
-            //CouchBase
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            #region CouchBase
+            services.AddCouchbase(configuration.GetSection("CouchBase"))
+                    .AddCouchbaseBucket<IEcommericeOrderBucketProvider>("e-commerce-order-system");
 
 
+            services.AddScoped<ICouchBaseContext, CouchBaseContext>()
+                    .AddScoped<IOrderDetailsRepositry, OrderDetailsRepositry>();
+
+
+            return services;
+            #endregion
+        }
+
+        public static void AddInfrastructureApp(this IApplicationBuilder app, IHostApplicationLifetime applicationLifetime)
+        {
+            applicationLifetime.ApplicationStopped.Register(() =>
+            {
+                app.ApplicationServices.GetRequiredService<ICouchbaseLifetimeService>();
+            });
         }
     }
 }
