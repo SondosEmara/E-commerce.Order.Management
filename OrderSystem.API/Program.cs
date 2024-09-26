e20f878af0f3bf1932e7ccac8dfbe83928b8d765
sondos emara <sondos.emara2002@gmail.com>
2024-09-26 16:01:51 +0300

    organize startup program file

--- OrderSystem.API/Program.cs
+++ OrderSystem.API/Program.cs
@@ -1,36 +1,65 @@
 
+using Microsoft.Extensions.Hosting.Internal;
+using OrderSystem.Infrastructure.Configuration;
+
 namespace OrderSystem.API
 {
     public class Program
     {
+     
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder=ConfigureServices(builder);
+            builder=ConfigureLayersServicesBuilder(builder);
 
             var app = builder.Build();
+            ConfigureLayersServicesApp(app);
+            ConfigureMiddleware(app);
+            app.Run();
+        }
 
-            // Configure the HTTP request pipeline.
+        private static void ConfigureLayersServicesApp(WebApplication app)
+        {
+            app.AddInfrastructureApp(app.Services.GetRequiredService<IHostApplicationLifetime>());
+        }
+        private static WebApplicationBuilder ConfigureLayersServicesBuilder(WebApplicationBuilder builder)
+        {
+            builder.Services.AddInfrastructureBuilder(builder.Configuration);
+            return builder;
+        }
+
+
+        private static void ConfigureMiddleware(WebApplication app)
+        {
             if (app.Environment.IsDevelopment())
             {
+                // Enable Swagger UI in development mode
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
+            // Enable HTTPS redirection
             app.UseHttpsRedirection();
 
+            // Enable Authorization middleware
             app.UseAuthorization();
 
-
+            // Map controller routes
             app.MapControllers();
-
-            app.Run();
         }
+
+       
+        private static WebApplicationBuilder ConfigureServices(WebApplicationBuilder builder)
+        {
+            // Add controllers
+            builder.Services.AddControllers();
+
+            // Configure Swagger for API documentation
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            return builder;
+        }
+
     }
 }
